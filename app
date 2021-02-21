6e74928a4ef20b0b43d6a7ea30678329373b9387
lamba <eclipjke@mail.ru>
2021-02-21 11:44:14 +0000

    Interactive

--- app
+++ app
@@ -2,6 +2,8 @@
 	
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$app = new \Symfony\Component\Console\Application();
+$app = new \Symfony\Component\Console\Application('interactive');
+
+$app->add(new \App\Interactive());
 
 $app->run();
---
+++ src/Interactive.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+
+
+class Interactive extends Command
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('interactive')
+		;
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {	    
+	    $helper = $this->getHelper('question');
+	    
+		$question1 = new Question('Введите ваше имя:');
+	    $name = $helper->ask($input, $output, $question1);
+	    
+	    $question2 = new Question('Введите ваш возраст:');
+	    $age = $helper->ask($input, $output, $question2);
+	    
+	    $question3 = new ChoiceQuestion(
+            'Выберите ваш пол:',
+            ['М', 'Ж'],
+            0
+        );
+        
+        $sex = $helper->ask($input, $output, $question3);
+	    
+	    $output->writeln('Здравствуйте, ' . $name . ' Ваш возраст:' . $age . ' Ваш пол: ' . $sex);
+
+        return Command::SUCCESS;
+    }
+}
